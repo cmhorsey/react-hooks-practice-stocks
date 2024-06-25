e8a896b4b7d0f72b00eb71b00e8e315c4e36899f
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-06-25 09:13:00 -0400

    Render all stocks to page

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,22 +1,34 @@
-import React from "react";
-import StockContainer from "./StockContainer";
-import PortfolioContainer from "./PortfolioContainer";
-import SearchBar from "./SearchBar";
+import React, { useState, useEffect } from "react"
+import StockContainer from "./StockContainer"
+import PortfolioContainer from "./PortfolioContainer"
+import SearchBar from "./SearchBar"
 
 function MainContainer() {
+  const [fetchTrigger, setFetchTrigger] = useState(false)
+  const [stocks, setStocks] = useState([])
+  //Set state with fetch
+  //Set trigger
+  //pass to Stock Container
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((response) => response.json())
+      .then(setStocks)
+  }, [fetchTrigger])
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} />
         </div>
         <div className="col-4">
           <PortfolioContainer />
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default MainContainer;
+export default MainContainer
